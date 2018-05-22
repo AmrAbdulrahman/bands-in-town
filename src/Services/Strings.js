742399486796f40d78e3ccc965681b0443fba6c2
Amr Abdulrahman <abu.muslim.amr@gmail.com>
2018-05-22 09:34:46 +0200

    Order Strings alphapitically

--- src/Services/Strings.js
+++ src/Services/Strings.js
@@ -2,21 +2,21 @@ import LocalizedStrings from 'react-localization';
 
 const Strings = new LocalizedStrings({
   en: {
-    navHeadline: 'Bands In Town',
-    search: 'Search',
-    homePlaceholderMessage: `There's nothing to see here, keep moving...`,
-    searchEvents: 'Search events',
-    from: 'From',
-    to: 'To',
-    searchPlaceholderMessage: `Enter an artist name, filter by dates, and let's see what BandsInTown has for you...`,
-    noResultsFound: 'No results found',
-    visitEventPage: 'Visit Event Page',
-    location: 'Location',
     bandsInTown: 'Bands In Town',
-    facebook: 'Facebook',
-    searchByArtistName: 'Search by artist name...',
     dateFromMustBeBeforeDateTo: '(From) date must be before (To) date',
+    facebook: 'Facebook',
     filterEventsByDate: 'Filter events by date',
+    from: 'From',
+    homePlaceholderMessage: `There's nothing to see here, keep moving...`,
+    location: 'Location',
+    navHeadline: 'Bands In Town',
+    noResultsFound: 'No results found',
+    search: 'Search',
+    searchEvents: 'Search events',
+    searchPlaceholderMessage: `Enter an artist name, filter by dates, and let's see what BandsInTown has for you...`,
+    searchByArtistName: 'Search by artist name...',
+    to: 'To',
+    visitEventPage: 'Visit Event Page',
   },
 });
 
